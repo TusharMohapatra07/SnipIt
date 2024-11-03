9f1c75d1c9ce3a346a65f02dfdbdd685cca0eb44
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2024-11-03 22:18:28 +0530

    add templating

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("server", "go")
-	w.Write([]byte("Hello from snippetbox"))
+	// w.Write([]byte("Hello from snippetbox"))
+
+	templ, err := template.ParseFiles("./ui/html/base.tmpl.html", "./ui/html/pages/home.tmpl.html")
+
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error 1", http.StatusInternalServerError)
+		return
+	}
+
+	err = templ.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
