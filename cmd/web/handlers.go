2512251ecac9b0c1c4fd1746cdeb82c1ff238661
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2024-11-28 22:36:40 +0530

    add dependency injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,48 +2,45 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("server", "go")
 	// w.Write([]byte("Hello from snippetbox"))
-
-	templ, err := template.ParseFiles("./ui/html/base.tmpl.html", "./ui/html/partials/nav.tmpl.html", "./ui/html/pages/home.tmpl.html")
+	files := []string{"./ui/html/base.tmpl.html", "./ui/html/partials/nav.tmpl.html", "./ui/html/pages/home.tmpl.html"}
+	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error 1", http.StatusInternalServerError)
+		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
-	err = templ.ExecuteTemplate(w, "base", nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
+
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
-
-	// msg := fmt.Sprintf("Display a specific snippet with id %d...", id)
-	// w.Write([]byte(msg))
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet...."))
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Creating and saving a new snippet...."))
+	w.Write([]byte("Save a new snippet..."))
 }
