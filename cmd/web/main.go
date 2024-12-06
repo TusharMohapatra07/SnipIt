27e6e135fb3232a07939fb4c32564b5b460d6d43
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2024-12-06 10:43:02 +0530

    add routes to a different file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,16 +24,7 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-
-	mux.Handle("GET /static/", http.StripPrefix("/static/", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux := app.routes()
 
 	logger.Info("Starting server", "addr", *addr)
 	err := http.ListenAndServe(*addr, mux)
