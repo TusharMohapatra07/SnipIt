35f4edb4e372daea0b9d3c7c2e2ee8d3d3db9189
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2024-12-21 21:54:46 +0530

    add db connection logic + rm Makefile

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
@@ -15,12 +16,19 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	dsn := flag.String("dsn", `postgresql://<username>:<password>@<host>:<port>/<database>?<parameters>`, "PostgreSQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		Level:     slog.LevelDebug,
 		AddSource: true,
 	}))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+	}
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
@@ -29,7 +37,20 @@ func main() {
 	mux := app.routes()
 
 	logger.Info("Starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err = http.ListenAndServe(*addr, mux)
 
 	logger.Error(err.Error())
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
