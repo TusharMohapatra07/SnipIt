c41de17b944773c0ac481d3e3a225497d4c4684b
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2024-12-29 03:14:39 +0530

    add funcmap

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"snippetbox/internal/models"
 )
@@ -10,6 +11,14 @@ import (
 type templateData struct {
 	Snippet  models.Snippet
 	Snippets []models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("01 Jan 2000 at 13:00")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -23,12 +32,12 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
 
-		ts, err = ts.ParseGlob("/ui/html/partials/*.tmpl.html")
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
