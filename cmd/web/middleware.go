d23102b2ce81a5b0b86a1c20150f44f544c204f1
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2025-02-11 12:11:59 +0530

    add csrf middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
 func commonHeaders(next http.Handler) http.Handler {
@@ -57,4 +59,15 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 		w.Header().Add("Cache-control", "no-store")
 		next.ServeHTTP(w, r)
 	})
+}
+
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+
+	return csrfHandler
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/joho/godotenv v1.5.1
 )
 
-require golang.org/x/crypto v0.32.0
-
-require github.com/justinas/alice v1.2.0 // indirect
+require (
+	github.com/justinas/alice v1.2.0
+	github.com/justinas/nosurf v1.1.1
+	golang.org/x/crypto v0.32.0
+)
